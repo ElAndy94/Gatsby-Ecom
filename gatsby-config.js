48b5912d4528e5c6af266e168b98e235c00c63dc
Andrew <andrewpeliza@hotmail.com>
2020-02-26 13:25:10 +0000

    added stripe setup to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,22 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-stripe`,
+    // {
+    //   resolve: `gatsby-source-stripe`,
+    //   options: {
+    //     objects: [
+    //       "Balance",
+    //       "BalanceTransaction",
+    //       "Product",
+    //       "ApplicationFee",
+    //       "Sku",
+    //       "Subscription",
+    //     ],
+    //     secretKey: "secretKeyHere",
+    //     downloadFiles: true,
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,16 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
+  let stripe
   useEffect(() => {
-    //document.title = "You clicked 10 times"
+    stripe = window.Stripe("pk_test_zaJJK5t95ONRd1bRGnYiX93R", {
+      betas: ["checkout_beta_4"],
+    })
   })
+
+  const redirectToCheckout = () => {
+    console.log("clicked")
+  }
 
   return (
     <Layout>
@@ -16,6 +23,15 @@ const IndexPage = () => {
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
+      <button
+        id="checkout-button"
+        role="link"
+        onClick={() => {
+          redirectToCheckout()
+        }}
+      >
+        BUY MY EBOOK
+      </button>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
